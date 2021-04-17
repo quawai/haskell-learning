8c0b4020be64aa20fa7aef8d23adefe715dfad25
Shin Quawai <quawai@me.com>
2021-04-17 09:57:11 +0900

    練習のコード

---
+++ exercise-codes/Params.hs
@@ -0,0 +1,4 @@
+module Params where
+x = "100"
+y = "200"
+z = "300"
---
+++ exercise-codes/myapp.hs
@@ -0,0 +1,9 @@
+module Main (main) where
+
+import MyApp.SomeModule
+
+
+main::IO()
+main = do
+  helloMyApp "Haskell"
+  byeMyApp "Others"
